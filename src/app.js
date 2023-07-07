0a341d92461c40ed0d296e9c7700cc5362878495
nata388 <128762188+nata388@users.noreply.github.com>
2023-07-07 19:41:55 +0000

    excuse terminado

--- src/app.js
+++ src/app.js
@@ -6,6 +6,23 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
+  document.querySelector("#excuse").innerHTML = generador();
   console.log("Hello Rigo from the console!");
+};
+let generador = () => {
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+  let x = Math.floor(Math.random() * who.length);
+  let y = Math.floor(Math.random() * action.length);
+  let g = Math.floor(Math.random() * what.length);
+  let n = Math.floor(Math.random() * when.length);
+  return who[x] + " " + action[y] + " " + what[g] + " " + when[n];
 };
